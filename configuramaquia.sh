438ec03470d32852e13f4e6961f44a4641ed9397
Jutair Rios <84020383+jutair@users.noreply.github.com>
2022-09-01 23:13:41 -0300

    Update configuramaquia.sh

--- configuramaquia.sh
+++ configuramaquia.sh
@@ -1,3 +1,2 @@
-wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py && chmod +x speedtest-cli && ./speedtest-cli && python speedtest-cli
-
+&wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py && chmod +x speedtest-cli && ./speedtest-cli && python speedtest-cli
 #wget https://releases.ubuntu.com/22.04.1/ubuntu-22.04.1-desktop-amd64.iso?_ga=2.202969367.785626495.1662080137-1769000709.1662080137
